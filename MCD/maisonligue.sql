f76721df96431d9d02a4d3884b44e26cfa8a0f45
SafiDial <safi076711@gmail.com>
2025-01-31 16:58:25 +0100

    Ajout du script sql

--- MCD/maisonligue.sql
+++ MCD/maisonligue.sql
@@ -1,3 +1,21 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 31, 2025 at 03:55 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `maisonligue`
@@ -6,45 +24,23 @@
 -- --------------------------------------------------------
 
 --
--- Table structure for table `appartenir`
---
-
-CREATE TABLE `appartenir` (
-  `idutilisateur` int NOT NULL,
-  `idligue` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `avoirunrole`
---
-
-CREATE TABLE `avoirunrole` (
-  `idutilisateur` int NOT NULL,
-  `idrole` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `ligue`
 --
 
 CREATE TABLE `ligue` (
-  `idligue` int NOT NULL,
-  `nomligue` varchar(50) DEFAULT NULL
+  `id_ligue` int NOT NULL,
+  `nom_ligue` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rôle`
+-- Table structure for table `role`
 --
 
-CREATE TABLE `rôle` (
-  `idrole` int NOT NULL,
-  `nomrole` varchar(50) DEFAULT NULL
+CREATE TABLE `role` (
+  `id_role` int NOT NULL,
+  `nom_role` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -54,14 +50,16 @@ CREATE TABLE `rôle` (
 --
 
 CREATE TABLE `utilisateur` (
-  `idutilisateur` int NOT NULL,
-  `nom` varchar(50) DEFAULT NULL,
-  `prénom` varchar(50) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `mot_depasse` varchar(50) DEFAULT NULL,
-  `typeutilisateur` varchar(50) DEFAULT NULL,
-  `datearrivée` date DEFAULT NULL,
-  `datedepart` date DEFAULT NULL
+  `id_utilisateur` int NOT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mot_de_passe` varchar(255) DEFAULT NULL,
+  `type_utilisateur` varchar(255) DEFAULT NULL,
+  `date_arrivee` date DEFAULT NULL,
+  `date_depart` date DEFAULT NULL,
+  `id_role` int NOT NULL,
+  `id_ligue` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -69,54 +67,35 @@ CREATE TABLE `utilisateur` (
 --
 
 --
--- Indexes for table `appartenir`
---
-ALTER TABLE `appartenir`
-  ADD PRIMARY KEY (`idutilisateur`,`idligue`),
-  ADD KEY `idligue` (`idligue`);
-
---
--- Indexes for table `avoirunrole`
---
-ALTER TABLE `avoirunrole`
-  ADD PRIMARY KEY (`idutilisateur`,`idrole`),
-  ADD KEY `idrole` (`idrole`);
-
---
 -- Indexes for table `ligue`
 --
 ALTER TABLE `ligue`
-  ADD PRIMARY KEY (`idligue`);
+  ADD PRIMARY KEY (`id_ligue`);
 
 --
--- Indexes for table `rôle`
+-- Indexes for table `role`
 --
-ALTER TABLE `rôle`
-  ADD PRIMARY KEY (`idrole`);
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id_role`);
 
 --
 -- Indexes for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`idutilisateur`);
+  ADD PRIMARY KEY (`id_utilisateur`),
+  ADD KEY `id_role` (`id_role`),
+  ADD KEY `id_ligue` (`id_ligue`);
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `appartenir`
+-- Constraints for table `utilisateur`
 --
-ALTER TABLE `appartenir`
-  ADD CONSTRAINT `appartenir_ibfk_1` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateur` (`idutilisateur`),
-  ADD CONSTRAINT `appartenir_ibfk_2` FOREIGN KEY (`idligue`) REFERENCES `ligue` (`idligue`);
-
---
--- Constraints for table `avoirunrole`
---
-ALTER TABLE `avoirunrole`
-  ADD CONSTRAINT `avoirunrole_ibfk_1` FOREIGN KEY (`idutilisateur`) REFERENCES `utilisateur` (`idutilisateur`),
-  ADD CONSTRAINT `avoirunrole_ibfk_2` FOREIGN KEY (`idrole`) REFERENCES `rôle` (`idrole`);
+ALTER TABLE `utilisateur`
+  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`),
+  ADD CONSTRAINT `utilisateur_ibfk_2` FOREIGN KEY (`id_ligue`) REFERENCES `ligue` (`id_ligue`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
